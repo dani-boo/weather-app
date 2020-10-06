f4eab6f4986f23531ee8195fde8fb970f4bdfb97
daniellebooysen1 <danielle.booysen1@vodafone.com>
2020-10-06 20:44:35 +0100

    rename tests

--- src/components/SixteenDayForecast/SixteenDayForecast.test.tsx
+++ src/components/SixteenDayForecast/SixteenDayForecast.test.tsx
@@ -4,7 +4,7 @@ import SixteenDayForecast from './SixteenDayForecast'
 import Weather from '../Weather/Weather'
 import Details from '../Details/Details'
 
-describe('CurrentForecast', () => {
+describe('SixteenDayForecast', () => {
   it('should render Weather and Details components', () => {
     const wrapper = shallow(<SixteenDayForecast/>)
     expect(wrapper.find(Weather)).toHaveLength(1)
